52984b9f5b95c0f33c939471dbb5cb4fca86ec6b
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-04-09 16:15:35 +0200

    refactor: optimize embedding computation by simplifying index calculations for token and position embeddings

--- phase3/phase3.cpp
+++ phase3/phase3.cpp
@@ -240,11 +240,14 @@ public:
     std::vector<float> embeddings(batch_size * context_len * embedding_dim, 0.0f);
     for (size_t b = 0; b < batch_size; ++b) {
       for (size_t c = 0; c < context_len; ++c) {
+        const size_t out_base = b * context_len * embedding_dim + c * embedding_dim;
+        const size_t token_id = ids[b * context_len + c];
+        const size_t tok_base = token_id * embedding_dim;
+        const size_t pos_base = c * embedding_dim;
+
         for (size_t i = 0; i < embedding_dim; ++i) {
-          embeddings[b * context_len * embedding_dim + c * embedding_dim + i] +=
-              token_embeddings.val[ids[c] * embedding_dim + i];
-          embeddings[b * context_len * embedding_dim + c * embedding_dim + i] +=
-              position_embeddings.val[c * embedding_dim + i];
+          embeddings[out_base + i] =
+              token_embeddings.val[tok_base + i] + position_embeddings.val[pos_base + i];
         }
       }
     }
