c273f3c31d93f6c3c306fdadfc23c58511d8f443
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-04-09 20:39:11 +0200

    refactor: optimize attention score calculation by introducing max_logits and sums_exp for numerical stability

--- phase3/phase3.cpp
+++ phase3/phase3.cpp
@@ -282,7 +282,7 @@ public:
       }
     }
 
-    std::vector<float> attention(batch_size * context_len * context_len, 0.0f);
+    std::vector<float> attention(batch_size * context_len * context_len);
     const float sqrt_hidden_dim = std::sqrt(static_cast<float>(head_dim));
 
     for (size_t b = 0; b < batch_size; ++b) {
@@ -307,6 +307,39 @@ public:
           }
         }
       }
+    }
+
+    std::vector<float> max_logits(batch_size * context_len);
+
+    for (size_t b = 0; b < batch_size; ++b) {
+      for (size_t i = 0; i < context_len; ++i) {
+        max_logits[b * context_len + i] =
+            attention[b * context_len * context_len + i * context_len];
+
+        for (size_t j = 0; j < context_len; ++j) {
+          max_logits[b * context_len + i] =
+              std::max(max_logits[b * context_len + i],
+                       attention[b * context_len * context_len + i * context_len + j]);
+        }
+      }
+    }
+
+    std::vector<double> sums_exp(batch_size * context_len, 0.0);
+
+    for (size_t b = 0; b < batch_size; ++b) {
+      for (size_t i = 0; i < context_len; ++i) {
+        for (size_t j = 0; j < context_len; ++j) {
+          sums_exp[b * context_len + i] += std::exp(
+              static_cast<double>(attention[b * context_len * context_len + i * context_len + j] -
+                                  max_logits[b * context_len + i]));
+        }
+      }
+    }
+
+    float loss_sum = 0.0f;
+    for (size_t b = 0; b < batch_size; ++b) {
+      loss_sum += static_cast<float>(max_logits[b] + std::log(sums_exp[b]) -
+                                     logits[b * vocab_size + targets[b]]);
     }
 
     // TODO
