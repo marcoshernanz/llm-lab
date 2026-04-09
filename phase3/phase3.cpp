34c741bdf6d8721586ba01208f51efe2d9795ed9
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-04-09 20:41:34 +0200

    refactor: implement stable attention score calculation using max_logits and sums_exp

--- phase3/phase3.cpp
+++ phase3/phase3.cpp
@@ -336,6 +336,17 @@ public:
       }
     }
 
+    for (size_t b = 0; b < batch_size; ++b) {
+      for (size_t i = 0; i < context_len; ++i) {
+        for (size_t j = 0; j < context_len; ++j) {
+          attention[b * context_len * context_len + i * context_len + j] =
+              std::exp(attention[b * context_len * context_len + i * context_len + j -
+                                 max_logits[b * context_len + i]]) /
+              sums_exp[b * context_len + i];
+        }
+      }
+    }
+
     float loss_sum = 0.0f;
     for (size_t b = 0; b < batch_size; ++b) {
       loss_sum += static_cast<float>(max_logits[b] + std::log(sums_exp[b]) -
