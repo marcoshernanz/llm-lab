4b73284a337047e4458de52de1b976951aed1ec8
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-08-19 21:35:39 -0700

    Refactor key projection in GlobalSelfAttention class to separate components for clarity

--- phase5/009_mla.py
+++ phase5/009_mla.py
@@ -197,8 +197,9 @@ class GlobalSelfAttention(nn.Module):
         q = torch.cat([q_c, q_r], dim=-1)
 
         kv_latent = self.kv_down_proj(x)  # [B, T, Dc]
-        k = torch.cat([self.k_up_proj(kv_latent), self.k_rope_proj(x)], dim=-1)
-        k = self.k_norm(split_heads(k, NUM_Q_HEADS))  # [B, Hq, T, Dh]
+        k_c = self.k_norm(split_heads(self.k_up_proj(kv_latent), NUM_Q_HEADS))  # [B, Hq, T, Dh]
+        k_r = self.k_norm(split_heads(self.k_rope_proj(x), NUM_Q_HEADS))  # [B, Hq, T, Dh]
+        k = torch.cat([k_c, k_r], dim=-1)
         v = split_heads(self.v_up_proj(kv_latent), NUM_Q_HEADS)  # [B, Hq, T, Dh]
 
         attn_output = combine_heads(attend(q, k, v, self.causal_mask))  # [B, T, D]
