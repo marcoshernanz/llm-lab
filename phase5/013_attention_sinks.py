e3517429f6e2ac56525b1d54fbc7802767e8af58
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-08-24 21:22:06 -0700

    Fix attend function to correctly remove the last attention weight dimension

--- phase5/013_attention_sinks.py
+++ phase5/013_attention_sinks.py
@@ -134,7 +134,7 @@ def attend(
     attn_scores = attn_scores.masked_fill(mask[:seq_len, :seq_len], -torch.inf)  # [B, Hq, T, T]
     attn_scores = torch.cat([attn_scores, z[:, None, None]], dim=-1)
     attn_weights = attn_scores.softmax(dim=-1)  # [B, Hq, T, T]
-    attn_weights.pop(dim=1)
+    attn_weights.pop(dim=-1)
     return attn_weights @ v  # [B, Hq, T, Dh]
 
 
