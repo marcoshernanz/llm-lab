01adab41956aa349db445dafd0b22ab107593dc9
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-08-29 15:12:56 -0700

    Add bias projection layer to KimiDeltaAttention class

--- phase5/015_gated_linear_attention.py
+++ phase5/015_gated_linear_attention.py
@@ -153,6 +153,7 @@ class KimiDeltaAttention(nn.Module):
         self.q_proj = nn.Linear(D_MODEL, D_MODEL, bias=False)
         self.k_proj = nn.Linear(D_MODEL, D_MODEL, bias=False)
         self.v_proj = nn.Linear(D_MODEL, D_MODEL, bias=False)
+
         self.b_proj = nn.Linear(D_MODEL, 1, bias=False)
         self.a_proj = nn.Linear(D_MODEL, D_MODEL, bias=False)
 
